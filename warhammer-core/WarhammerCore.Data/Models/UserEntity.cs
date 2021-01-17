e3b56835fc8cfdba803e86288d7a46bb6b0151de
Grzegorz <grzegorz.sobocinski2397@gmail.com>
2021-01-17 11:11:10 +0100

    Create entities for User

---
+++ warhammer-core/WarhammerCore.Data/Models/UserEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WarhammerCore.Data.Models
+{
+    public partial class UserEntity
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
