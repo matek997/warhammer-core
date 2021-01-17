50a393dde22339df90afc74ef2d401a49478c3c4
Mateusz Hanzel <matek997@outlook.com>
2021-01-17 13:33:07 +0100

    Add controller with signin action

---
+++ warhammer-core/WarhammerCore.WebApi/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WarhammerCore.Abstract.Interfaces;
+using WarhammerCore.Abstract.Models;
+using WarhammerCore.WebApi.Models.Enums;
+using WarhammerCore.WebApi.Models.Request;
+using WarhammerCore.WebApi.Models.Response;
+
+namespace WarhammerCore.WebApi.Controllers
+{
+    public class UserController : ApiControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService ?? throw new ArgumentNullException(nameof(userService));
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<SigninResponse>> Signin(SigninRequest request)
+        {
+            var token = await _userService.SignInAsync(request.Email, request.Password);
+
+            return new SigninResponse { Email = request.Email, Token = token.Token };
+        }
+    }
+}
