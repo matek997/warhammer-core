dc538b89d82c548a507835ad3c5eda7dbba2b4fe
Mateusz Hanzel <46403984+matek997@users.noreply.github.com>
2021-01-25 13:19:26 +0100

    Revert "Feature/user controller"

--- warhammer-core/WarhammerCore.WebApi/Startup.cs
+++ warhammer-core/WarhammerCore.WebApi/Startup.cs
@@ -7,24 +7,18 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 using System.IO;
-using System.Text;
 using WarhammerCore.Data.Models;
 using WarhammerCore.WebApi.Extensions;
 using WarhammerCore.WebApi.Validation;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace WarhammerCore.WebApi
 {
     public class Startup
     {
-        static public IConfiguration Config;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            Config = configuration;
-
         }
 
         public IConfiguration Configuration { get; }
@@ -45,21 +39,6 @@ namespace WarhammerCore.WebApi
             services.AddDbContext<WarhammerDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SqlServer")));
             services.AddSwaggerGen();
             services.AddAppServices();
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                ValidIssuer = Configuration["Jwt:Issuer"],
-                ValidAudience = Configuration["Jwt:Issuer"],
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
-                };
-                });
-            services.AddSingleton<IConfiguration>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -81,8 +60,6 @@ namespace WarhammerCore.WebApi
             app.UseRouting();
 
             app.UseAuthorization();
-
-            app.UseAuthentication();
 
             app.UseErrorHandling();
 
