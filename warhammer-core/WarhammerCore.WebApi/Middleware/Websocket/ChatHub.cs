c7685029f9e00db375c6cd568615b38d3a11898f
Mateusz Hanzel <matek997@outlook.com>
2021-02-20 05:49:41 +0100

    comms working

--- warhammer-core/WarhammerCore.WebApi/Middleware/Websocket/ChatHub.cs
+++ warhammer-core/WarhammerCore.WebApi/Middleware/Websocket/ChatHub.cs
@@ -1,14 +1,38 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SignalRChat.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task NewMessage(long username, string message)
-        {
-            await Clients.All.SendAsync("messageReceived", username, message);
-        }
+        public Task BroadcastMessage( string message) =>
+          Clients.All.SendAsync("broadcastMessage", message);
+
+        public Task Echo( string message) =>
+            Clients.Client(Context.ConnectionId)
+                   .SendAsync("echo", $"{message} (echo from server)");
+
+        /*  public override  Task OnConnectedAsync()
+          {
+              Groups.AddToGroupAsync(Context.ConnectionId, "messageReceived");
+
+              Clients.All.SendAsync("messageReceived", DateTime.Now.ToString());
+
+
+               return base.OnConnectedAsync();
+          }
+          public async Task NewMessage(long username, string message)
+          {
+              await Clients.All.SendAsync("messageReceived", username, message);
+          }*/
+        /*      public async Task NewMessage()
+              {
+                  var context = GlobalHost.ConnectionManager.GetHubContext<Status>();
+                  var subscribers = context.Clients;
+                  var subscriber = subscribers.Group("foo");
+                  subscriber.messageReceived("ello");
+              }*/
     }
 }
